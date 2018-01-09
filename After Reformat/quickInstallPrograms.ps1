334dc741f60b3d20cf0b0a17d48b91053fafa616
Amit Lin <amitlin.dev@gmail.com>
2018-01-09 23:25:15 +0200

    Update quickInstallPrograms.ps1

--- After Reformat/quickInstallPrograms.ps1
+++ After Reformat/quickInstallPrograms.ps1
@@ -1,3 +1,15 @@
+& {
+ $wid=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+ $prp=new-object System.Security.Principal.WindowsPrincipal($wid)
+ $adm=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+ $IsAdmin=$prp.IsInRole($adm)
+ if (-not $IsAdmin)
+ {
+    write-host "You must run this script in an elevated shell!"
+    exit
+ }
+}
+
 $programs = "7zip.install", "waterfox", "putty.install", "winscp.install", "sublimetext3", "nodejs.install", "sysinternals", "atom", "gitkraken", "vagrant", "docker-for-windows"
 
 write-host "Installing chocolatey..."

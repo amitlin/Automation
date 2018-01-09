187348dd32fe8a99b0dd2e904776ddb08d007a15
Amit Lin <amitlin.dev@gmail.com>
2018-01-09 23:19:59 +0200

    Create quickInstallPrograms.ps1

---
+++ After Reboot/quickInstallPrograms.ps1
@@ -0,0 +1,9 @@
+$programs = "7zip.install", "putty.install", "winscp.install", "sublimetext3", "nodejs.install", "sysinternals", "atom", "gitkraken", "vagrant", "docker-for-windows"
+
+write-host "Installing chocolatey..."
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+write-host "Chocolatey installed!"
+
+$installString = $programs -join " "
+
+echo y | choco install -y $installString

8c6d6e67fb6d561c3f8238c690e569076817641c
Amit Lin <amitlin.dev@gmail.com>
2018-05-18 19:56:22 +0300

    task bar customization

---
+++ After Reformat/Customization.ps1
@@ -0,0 +1,11 @@
+﻿Write-Host "Setting quick access shortcut to My PC"
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "LaunchTo" -Value 1 -PropertyType DWORD -Force
+
+Write-Host "Removing task button"
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Value 0 -PropertyType DWORD -Force
+
+Write-Host "Removing People button"
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" -Name "PeopleBand" -Value 0 -PropertyType DWORD -Force
+
+Write-Host "Removing search button"
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -Value 0 -PropertyType DWORD -Force
